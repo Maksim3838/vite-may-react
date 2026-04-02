2d07b060cc9752be3f729454e835dd021076bd55
Maksim <70702008m@gmail.com>
2026-04-02 12:42:38 +0300

    text

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,10 @@
+
+import css from "./App.module.css"
+import CafeInfo from "../CafeInfo/CafeInfo"
+
+
 export default function App() {
-    return
-    <div  className={css.app}></div>
+    return (<div className={css.app}>
+        <CafeInfo />
+    </div>);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,14 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.tsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './components/App/App'
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
 
-// main.tsx
 
-import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <div>
-    <h1>Welcome to React</h1>
-    <p>
-      This is JSX — it looks like HTML, but it's not quite the same. It has its
-      own rules!
-    </p>
-  </div>
-);
+
